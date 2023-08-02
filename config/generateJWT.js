79b8bf989632adf87cdf2b1290ef1ed9f5a73b6a
my6056 <129304542+my6056@users.noreply.github.com>
2023-08-02 18:06:46 +0000

    Update generateJWT.js

--- config/generateJWT.js
+++ config/generateJWT.js
@@ -3,8 +3,13 @@ const jwt = require("jsonwebtoken");
 const secretKey = process.env.SECRET_KEY;
 
 // export the generateToken function
-module.exports.generateJwtToken = async (userId, fullName, emailId) => {
-  return await jwt.sign({ userId, fullName, emailId }, secretKey, {
+module.exports.generateJwtToken = async (
+  userId,
+  lastName,
+  firstName,
+  email
+) => {
+  return await jwt.sign({ userId, firstName, lastName, email }, secretKey, {
     expiresIn: "24h",
   });
 };
